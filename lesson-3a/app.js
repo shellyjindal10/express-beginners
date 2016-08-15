f4b0d8182682667c902cb54e9979f37ad5955454
Shelly Jindal <sjindal@C02PHGACG3QC.local>
2016-08-15 16:12:52 -0700

    give single response for all requested parameters

---
+++ lesson-3a/app.js
@@ -0,0 +1,16 @@
+//create an express app
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 9000
+
+//create express middleware
+app.use(function(req, res) {
+  //it can take any requested url and give the same response
+  // try with localhost:9000/does/it/matter or localhost:9000
+  var data = '<h1>This can be take any URL</h1>'
+
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end(data)
+})
+
+app.listen(port)
