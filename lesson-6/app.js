5654fc74192e29448cd75f6bf9700985c3df237b
Shelly Jindal <sjindal@C02PHGACG3QC.local>
2016-08-17 00:15:34 -0700

    lesson 6

---
+++ lesson-6/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var express = require('express'),
+      posts = require('./mock/post.json'),
+       port = process.env.PORT || 9000
+
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('<h1>Hello There !!!</h1>');
+})
+
+app.get('/cake', function(req, res) {
+  res.send(posts);
+})
+
+app.listen(port, function() {
+  //callback function to display port number 
+	 console.log('The server is running on port => '+port);
+})
