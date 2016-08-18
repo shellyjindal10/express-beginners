2a17f0be2e0533fdee1da0d2e1d27d60c5600d68
Shelly Jindal <sjindal@C02PHGACG3QC.local>
2016-08-17 23:43:38 -0700

    adding comments

--- schemas-generator/app.js
+++ schemas-generator/app.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var express = require('express'),
-    GenerateSchema = require('generate-schema'),
-    port = process.env.PORT || 9000,
-    app = express()
+    GenerateSchema = require('generate-schema'), //Library for converting json to json schema
+    port = process.env.PORT || 9000, //port number
+    app = express(),
+    posts = require('./mock/base') //default json
 
 
 app.get('/', function(req, res) {
@@ -11,41 +12,12 @@ app.get('/', function(req, res) {
 })
 
 app.get('/schema', function(req, res) {
-	var schema = GenerateSchema.json('Product', [
-    {
-        "id": 2,
-        "name": "An ice sculpture",
-        "price": 12.50,
-        "tags": ["cold", "ice"],
-        "dimensions": {
-            "length": 7.0,
-            "width": 12.0,
-            "height": 9.5
-        },
-        "warehouseLocation": {
-            "latitude": -78.75,
-            "longitude": 20.4
-        }
-    },
-    {
-        "id": 3,
-        "name": "A blue mouse",
-        "price": 25.50,
-        "dimensions": {
-            "length": 3.1,
-            "width": 1.0,
-            "height": 1.0
-        },
-        "warehouseLocation": {
-            "latitude": 54.4,
-            "longitude": -32.7
-        }
-    }
-])
-  res.send(schema);
+  //generating json schema from json 
+  var schema = GenerateSchema.json('Product', [posts])
+  res.send(schema); //posting the result as response.
 })
 
 app.listen(port, function() {
   //callback function to display port number 
-	 console.log('The server is running on port => '+port);
+  console.log('The server is running on port => '+port);
 })
